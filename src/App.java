f739b935eddcfdc825aad2d4443399de9d3dbf80
Alex Lambert <hyperactive@live.com>
2022-05-17 09:51:10 -0700

    Added the scanner and made it work for both files

--- src/App.java
+++ src/App.java
@@ -20,13 +20,44 @@ public class App {
 
         Connection conn;
         conn = DriverManager.getConnection(url, props);
-
         Statement st = conn.createStatement();
+
+        conn.setAutoCommit(false);
+
+        Scanner makeDB = new Scanner(new File("env/icecreamDB.txt"));
+        makeDB.useDelimiter(";");
+
+        while (makeDB.hasNext()) {
+            String nextLine = makeDB.next() + ";";
+            System.out.println(nextLine);
+            st.execute(nextLine);
+        }
+
+        Scanner makePreloaded = new Scanner(
+                new File("env/preloadedQueries.txt"));
+        makePreloaded.useDelimiter(";");
+
+        while (makePreloaded.hasNext()) {
+            String nextLine = makePreloaded.next() + ";";
+            System.out.println(nextLine);
+            st.execute(nextLine);
+        }
+
+        ResultSet prepRS = st.executeQuery("EXECUTE getEmployees;");
+        while (prepRS.next()) {
+            System.out.print("Column 1 returned ");
+            System.out.println(
+                    prepRS.getString(1) + " | " + prepRS.getString(2));
+        }
+
         ResultSet rs = st.executeQuery("SELECT * FROM Employee");
         while (rs.next()) {
             System.out.print("Column 1 returned ");
             System.out.println(rs.getString(1) + " | " + rs.getString(2));
         }
+
+        conn.rollback();
+
         rs.close();
         st.close();
     }
