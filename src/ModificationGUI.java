1c8be549bbad7f16293a6ab680d007fa4bcd7ef1
Josh Helzerman <helzee@uw.edu>
2022-05-30 15:59:22 -0700

    modifications add!

--- src/ModificationGUI.java
+++ src/ModificationGUI.java
@@ -1,25 +1,30 @@
 import java.awt.*;
 import java.awt.event.*;
+
+import javax.naming.spi.DirStateFactory.Result;
 import javax.swing.*;
 import java.sql.*;
 import java.util.*;
 import java.math.BigDecimal;
 
 public class ModificationGUI {
-   private static String[] cols = { "categoryID", "currentPrice", "name",
-         "description", "isoffered" };
-   private static String[] types = { "integer", "numeric", "string", "string",
+   private static String[] cols = { "ItemID", "unitsNeeded", "currentPrice",
+         "name", "isoffered" };
+   private static String[] types = { "integer", "numeric", "numeric", "string",
          "boolean" };
 
-   private static String entity = "menuproduct";
-   private static JFrame frame;
+   private static int itemID;
 
-   public static JPanel createAddModificationCard() {
+   public static JPanel createAddModificationCard() throws SQLException {
+      itemID = -1;
 
       JPanel addProduct = new JPanel();
       JTextField[] info = new JTextField[cols.length];
 
-      for (int i = 0; i < cols.length - 1; i++) {
+      JPanel itemButtons = getItems();
+      addProduct.add(itemButtons);
+
+      for (int i = 1; i < cols.length - 1; i++) {
          int size = 32;
          if (types[i].toLowerCase().equals("string")) {
             size = 40;
@@ -39,9 +44,14 @@ public class ModificationGUI {
 
       enterButton.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-            Insert.insertMenuProduct(256,
-                  Double.parseDouble(info[1].getText()), info[2].getText(),
-                  info[3].getText());
+            if (itemID > -1) {
+               Insert.insertMenuMod(itemID,
+                     Double.parseDouble(info[1].getText()),
+                     Double.parseDouble(info[2].getText()), info[3].getText());
+            } else {
+               System.err.println(
+                     "Error: Must select an item for the modification");
+            }
 
             try {
                App.conn.commit();
@@ -54,6 +64,47 @@ public class ModificationGUI {
       });
       addProduct.add(enterButton);
       return addProduct;
+   }
+
+   private static JPanel getItems() throws SQLException {
+      JPanel editItem = new JPanel();
+
+      String query = "SELECT * FROM Item;";
+      ResultSet items = Execute.runQuery(query);
+
+      Vector<JRadioButton> itemButtons = new Vector<JRadioButton>();
+      Vector<Integer> itemIDs = new Vector<Integer>();
+      JRadioButton button = null;
+      while (items.next()) {
+         itemIDs.add(items.getInt(1));
+         itemButtons.add(button = new JRadioButton(items.getString(2)));
+         button.putClientProperty("id", items.getInt(1));
+
+      }
+
+      ButtonGroup buttGroup = new ButtonGroup();
+      for (JRadioButton b : itemButtons) {
+         buttGroup.add(b);
+         editItem.add(b);
+      }
+
+      for (int i = 0; i < itemButtons.size(); i++) {
+         itemButtons.get(i).addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+               itemID = ((int) ((JRadioButton) e.getSource())
+                     .getClientProperty("id"));
+
+            }
+         });
+      }
+
+      return editItem;
+
+   }
+
+   private static void addItem(int id, int modID) {
+
    }
 
    public static JPanel createEditModificationCard() {
